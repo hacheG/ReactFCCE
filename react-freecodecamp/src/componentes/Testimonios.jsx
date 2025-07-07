6fb7f04a52c8cde3bbf17c2f4ee1af4e35c86f3c
hacheTresComaCatorce <hatrujillom@unal.edu.co>
2025-07-07 17:05:54 -0500

    add photos and directories

---
+++ react-freecodecamp/src/componentes/Testimonios.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import imagen from "../imagenes/testimonio-Shawn.webp"
+
+function Testimonios(){
+    return(
+        <div className="contenedor-testimonio">
+            <img src={imagen} alt="Foto Shawn" />
+        </div>
+    )
+}
+
+export default Testimonios
