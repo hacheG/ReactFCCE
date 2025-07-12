e2ee1e34d3a09e5a31200f33221e288b46ec5810
hacheTresComaCatorce <hatrujillom@unal.edu.co>
2025-07-12 13:24:02 -0500

    add testimonial card

--- react-freecodecamp/src/componentes/Testimonio.jsx
+++ react-freecodecamp/src/componentes/Testimonio.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import imagen from "../imagenes/testimonio-Emma.webp";
 import "../hojas-de-estilos/Testimonio.css"
 
-function Testimonio(){
+function Testimonio(props){
     return(
         <div className="contenedor-testimonio">
             
-            <img className="imagen-testimonio" src={imagen} alt="Foto Emma" />
-            <div className="contenedor-texto-testimonios"></div>
-            <p className="nombre-testimonio">Emma Bostian en Suecia</p>
-            <p className="cargo-testimonio">Ingeniera de software en spotyfy</p>
-            <p className="texto-testimonio">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci ducimus fugit saepe, voluptatum beatae at voluptatibus sunt aut consectetur inventore quia officia possimus cupiditate rerum quod ullam. Repellendus, rem minus?s</p>
+            <img className="imagen-testimonio" src={props.imagenEmma} alt="Foto Emma" />
+            <div className="contenedor-texto-testimonios">
+                <p className="nombre-testimonio">{props.nombre} en {props.pais}</p>
+                <p className="cargo-testimonio">{props.cargo} en {props.empresa}</p>
+                <p className="texto-testimonio">"{props.testimonio}"</p>
+
+            </div>
         </div>
     )
 }
