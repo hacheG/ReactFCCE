c6007d12111bfb3b32264f0af8b3c87536464824
hacheTresComaCatorce <hatrujillom@unal.edu.co>
2025-07-12 19:55:10 -0500

    add all project

--- click-counter/src/App.jsx
+++ click-counter/src/App.jsx
@@ -1,16 +1,41 @@
 import { useState } from 'react'
-import './App.css'
+import "./App.css"
+import Boton from "./componentes/Boton"
+import Contador from './componentes/Contador'
 import freeCodeCamp from "./imagenes/freecodecamp-logo.png"
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [numClics, setnumClics] = useState(0)
+
+  const manejarClic = () => {
+    setnumClics( numClics + 1 )
+  }
+
+  const reiniciarContador = () => {
+    setnumClics(0);
+  }
 
   return (
     <div className='app'>
       <div className='freecodecamp-logo-contenedor'>
         <img className='frecodecamp-logo' src={freeCodeCamp} alt="Logo de Freecodecamp" />
       </div>
-      <div className='contenedor-principal'></div>
+      <div className='contenedor-principal'>
+        <Contador
+          numClics={numClics}
+        />
+        <Boton 
+          texto = "Click"
+          esBotonDeClic = {true}
+          manejarClic = {manejarClic}
+          />
+        
+        <Boton 
+          texto = "Reiniciar"
+          esBotonDeClic = {false}
+          manejarClic = {reiniciarContador}
+          />
+      </div>
     </div>
   )
 }
